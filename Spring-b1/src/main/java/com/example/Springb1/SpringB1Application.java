13122ae1b2dcbe89209f87822bac21d00e9815a4
Sshruti24 <dhanashree.karambalkar@atcs.com>
2021-02-03 18:55:04 +0530

    Communication

---
+++ Spring-b1/src/main/java/com/example/Springb1/SpringB1Application.java
@@ -0,0 +1,13 @@
+package com.example.Springb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringB1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringB1Application.class, args);
+	}
+
+}
---
+++ Spring-b1/src/test/java/com/example/Springb1/SpringB1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringB1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
